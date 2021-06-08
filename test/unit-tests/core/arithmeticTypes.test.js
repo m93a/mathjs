dee06ef2ad800f73e77175b46f2aa683d0b17e1e
Michal Grno <m93a.cz@gmail.com>
2021-06-08 12:19:17 +0200

    added tests for Quaternion.js as a custom type

---
+++ test/unit-tests/core/arithmeticTypes.test.js
@@ -0,0 +1,17 @@
+import approx from '../../../tools/approx.js'
+import math from '../../../src/defaultInstance.js'
+import Quaternion from '@m93a/quaternion.js'
+
+const { abs, log, exp } = math
+
+describe('arithmetic types', () => {
+  it('supports basic functions on Quaternion.js', () => {
+    const q = Quaternion(1, 2, 3, 4)
+
+    approx.deepEqual(abs(q), 5.477225575051661)
+
+    approx.deepEqual(log(q), Quaternion(1.7005986908310777, 0.515190292664085, 0.7727854389961275, 1.03038058532817))
+
+    approx.deepEqual(exp(q), Quaternion(1.6939227236832994, -0.7895596245415587, -1.184339436812338, -1.5791192490831174))
+  })
+})
